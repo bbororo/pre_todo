56ecb3da2188570c5ae907745a5701e890df0a7e
bbororo <koogoori54@gmail.com>
2022-10-21 12:48:41 +0900

    upload

---
+++ src/main/java/com/example/pre_todo_intelli/PreTodoIntelliApplication.java
@@ -0,0 +1,13 @@
+package com.example.pre_todo_intelli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreTodoIntelliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreTodoIntelliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pre_todo_intelli/PreTodoIntelliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pre_todo_intelli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreTodoIntelliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
